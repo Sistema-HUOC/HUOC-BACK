b4f84c6404892eb76edc3434ac5dc278ad5e49b1
JairAssisDev <jairv141@gmail.com>
2025-03-28 21:33:22 -0300

    📦 chore: Criação do projeto spring e config do swagger

---
+++ src/main/java/br/com/HUOC_BACK/HuocBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.HUOC_BACK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuocBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuocBackApplication.class, args);
+	}
+
+}
