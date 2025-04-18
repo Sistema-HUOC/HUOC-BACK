4e20abb44bd33d4517a2cedc798fda3e6582e3b4
JairAssisDev <63867699+JairAssisDev@users.noreply.github.com>
2025-04-18 11:58:47 -0300

    Update CategoriaSintomaRepository.java

--- src/main/java/br/com/HUOC_BACK/repository/CategoriaSintomaRepository.java
+++ src/main/java/br/com/HUOC_BACK/repository/CategoriaSintomaRepository.java
@@ -1,11 +1,9 @@
 package br.com.HUOC_BACK.repository;
 
-
 import br.com.HUOC_BACK.model.CategoriaSintoma;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.UUID;
 
 @Repository
