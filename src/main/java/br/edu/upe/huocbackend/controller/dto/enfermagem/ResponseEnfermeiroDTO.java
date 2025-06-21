f1dbf7b17a38400d0736de43995030ef5bdf0858
JairAssisDev <63867699+JairAssisDev@users.noreply.github.com>
2025-06-21 15:52:37 -0300

    fix/mudanças (#65)

--- src/main/java/br/edu/upe/huocbackend/controller/dto/enfermagem/ResponseEnfermeiroDTO.java
+++ src/main/java/br/edu/upe/huocbackend/controller/dto/enfermagem/ResponseEnfermeiroDTO.java
@@ -2,9 +2,9 @@ package br.edu.upe.huocbackend.controller.dto.enfermagem;
 
 import br.edu.upe.huocbackend.model.Enfermagem;
 
-public record ResponseEnfermeiroDto(String nome, String cpf, String email,String coren) {
+public record ResponseEnfermeiroDTO(String nome, String cpf, String email, String coren) {
 
-    public ResponseEnfermeiroDto(Enfermagem enfermagem) {
+    public ResponseEnfermeiroDTO(Enfermagem enfermagem) {
         this(enfermagem.getNome(), enfermagem.getCpf(), enfermagem.getEmail(), enfermagem.getCoren());
     }
 }
