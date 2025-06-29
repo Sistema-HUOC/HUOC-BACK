629966d807f58a3f4a4c1d1e6e901f3ace9944ee
jairassisdev <jairv141@gmail.com>
2025-06-29 00:32:42 -0300

    fix/mudanças

--- src/main/java/br/edu/upe/huocbackend/controller/InstituicaoController.java
+++ src/main/java/br/edu/upe/huocbackend/controller/InstituicaoController.java
@@ -1,16 +1,18 @@
 package br.edu.upe.huocbackend.controller;
 
-import br.edu.upe.huocbackend.controller.dto.instituicao.InstituicaoDto;
+import br.edu.upe.huocbackend.controller.dto.instituicao.CreateInstituicaoDto;
+import br.edu.upe.huocbackend.controller.dto.instituicao.ResponseInstituicaoDto;
+import br.edu.upe.huocbackend.controller.dto.instituicao.UpdateInstituicaoDto;
 import br.edu.upe.huocbackend.service.InstituicaoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/instituicoes")
@@ -26,9 +28,31 @@ public class InstituicaoController {
     @Operation(summary = "Criação de Instituição", description = "Cria uma nova instituição com o nome fornecido.")
     @ApiResponse(responseCode = "201", description = "Instituição criada com sucesso")
     @ApiResponse(responseCode = "400", description = "Requisição inválida. Verifique os campos obrigatórios.")
-    public ResponseEntity<Void> createInstituicao(@Valid @RequestBody InstituicaoDto dto) {
+    public ResponseEntity<Void> createInstituicao(@Valid @RequestBody CreateInstituicaoDto dto) {
         instituicaoService.createInstituicao(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @GetMapping
+    @Operation(summary = "Listar Instituições", description = "Lista instituições com base nos filtros opcionais: nome da instituição, nome do campus e CNPJ.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de instituições retornada com sucesso")
+    })
+    public ResponseEntity<List<ResponseInstituicaoDto>> getInstituicao(@RequestParam(required = false) String nomeInstituicao,
+                                                                       @RequestParam(required = false) String nomeDoCampos,
+                                                                       @RequestParam(required = false) String cnpj) {
+        List<ResponseInstituicaoDto> instituicaoDtos = instituicaoService.listInstituicaos(nomeInstituicao,nomeDoCampos,cnpj);
+        return ResponseEntity.status(HttpStatus.OK).body(instituicaoDtos);
+    }
+    @PutMapping
+    @Operation(summary = "Atualizar Instituição", description = "Atualiza uma instituição existente com os novos dados fornecidos.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Instituição atualizada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida. Verifique os campos obrigatórios."),
+            @ApiResponse(responseCode = "404", description = "Instituição não encontrada")
+    })
+    public ResponseEntity<Void> UpdateInstituicao(@Valid @RequestBody UpdateInstituicaoDto dto) {
+        instituicaoService.updateInstituicao(dto);
+        return ResponseEntity.noContent().build();
+    }
 }
---
+++ src/main/java/br/edu/upe/huocbackend/controller/dto/instituicao/CreateInstituicaoDto.java
@@ -0,0 +1,19 @@
+package br.edu.upe.huocbackend.controller.dto.instituicao;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+public record CreateInstituicaoDto(
+        @NotBlank(message = "O nome da instituição é obrigatório")
+        @Schema(description = "Nome da instituição", example = "Universidade Federal de Pernambuco")
+        String nomeInstituicao,
+        @NotBlank(message = "O nome do Campos é obrigatório")
+        @Schema(description = "Nome do Campos", example = "Palmares")
+        String nomeDoCampos,
+        @NotBlank(message = "O CNPJ é obrigatório")
+       // @CNPJ(message = "CNPJ inválido")
+        @Schema(description = "CNPJ da instituição", example = "12.345.678/0001-90")
+        String cnpj
+    ) {
+}
